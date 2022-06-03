cc56db7f292a8845d1c56b6d230560e4014ed415
Eddie Xiao <ex47222@gmail.com>
2022-06-03 18:46:21 -0400

    qtile config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -48,17 +48,19 @@ keys = [
     Key([mod], "p", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
 
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    # Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-    #     desc="Move window to the left"),
-    # Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-    #     desc="Move window to the right"),
-    # Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-    #     desc="Move window down"),
-    # Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    Key([mod, "shift"], "j",
+        lazy.layout.shuffle_down(),
+        lazy.layout.section_down(),
+        desc='Move windows down in current stack'
+        ),
+    Key([mod, "shift"], "k",
+        lazy.layout.shuffle_up(),
+        lazy.layout.section_up(),
+        desc='Move windows up in current stack'
+        ),
 
     # Grow windows. If current window is on the edge of screen and direction
+
     Key([mod, "control"], "h", lazy.layout.grow_left(),
         desc="Grow window to the left"),
     Key([mod, "control"], "l", lazy.layout.grow_right(),
@@ -66,43 +68,26 @@ keys = [
     Key([mod, "control"], "j", lazy.layout.grow_down(),
         desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+
+
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
-    Key([mod], "n",
-        lazy.layout.normalize(),
-        desc='normalize window size ratios'
-        ),
-    Key([mod], "m",
-        lazy.layout.maximize(),
-        desc='toggle window between minimum and maximum sizes'
-        ),
-    Key([mod, "shift"], "f",
-        lazy.window.toggle_floating(),
-        desc='toggle floating'
-        ),
-    Key([mod], "f",
-        lazy.window.toggle_fullscreen(),
-        desc='toggle fullscreen'
-        ),
+    Key([mod], "m", lazy.layout.maximize(), desc='toggle window between minimum and maximum sizes'),
+    Key([mod, "shift"], "f", lazy.window.toggle_floating(), desc='toggle floating'),
+    Key([mod], "f", lazy.window.toggle_fullscreen(), desc='toggle fullscreen'),
+
+    Key([mod, "shift"], "h", lazy.layout.swap_left()),
+    Key([mod, "shift"], "l", lazy.layout.swap_right()),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down()),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up()),
+
     # Tree tabs stuff 
-    Key([mod, "shift"], "h",
-    lazy.layout.move_left(),
-    desc='Move up a section in treetab'
-    ),
-    Key([mod, "shift"], "l",
-    lazy.layout.move_right(),
-    desc='Move down a section in treetab'
-    ),
+    # Key([mod, "shift"], "h", lazy.layout.move_left(), desc='Move up a section in treetab'),
+    # Key([mod, "shift"], "l", lazy.layout.move_right(), desc='Move down a section in treetab'),
 
     # Monitor support 
-    Key([mod], "w",
-    lazy.to_screen(0),
-    desc='Keyboard focus to monitor 1'
-    ),
-    Key([mod], "e",
-    lazy.to_screen(1),
-    desc='Keyboard focus to monitor 2'
-    ),
+    Key([mod], "w", lazy.to_screen(0), desc='Keyboard focus to monitor 1'),
+    Key([mod], "e", lazy.to_screen(1), desc='Keyboard focus to monitor 2'),
 
     # Launchers 
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
@@ -116,25 +101,28 @@ keys = [
     Key([mod], "r", lazy.spawn('rofi -show drun -show-icons '), desc="Run rofi"),
     Key([mod], "i", lazy.spawn("rofi -show window")),
 
+    # Screenshotter 
+    Key([], "Print", lazy.spawn("flameshot gui")),
+
 ]
 
 
 groups = [Group(i) for i in "1234567890"]
 group_names = [("", {'layout': 'monadtall' }),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("ﭮ", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'monadtall'}),
-               ("", {'layout': 'floating'})]
+    ("", {'layout': 'monadtall'}),
+    ("", {'layout': 'monadtall'}),
+    ("", {'layout': 'monadtall'}),
+    ("", {'layout': 'monadtall'}),
+    ("ﭮ", {'layout': 'monadtall'}),
+    ("", {'layout': 'monadtall'}),
+    ("", {'layout': 'monadtall'}),
+    ("", {'layout': 'floating'})]
 
 
 groups = [Group(name, **kwargs) for name, kwargs in group_names]
 
 for i, (name, kwargs) in enumerate(group_names, 1):
-    keys.append(Key([mod], str(i), lazy.group[name].toscreen()))        # Switch to another group
+    keys.append(Key([mod], str(i), lazy.group[name].toscreen(toggle=True)))        # Switch to another group
     keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name))) # Send current window to another group
 
 
@@ -154,10 +142,10 @@ for i, (name, kwargs) in enumerate(group_names, 1):
 #     ])
 
 layout_theme = {"border_width": 3,
-                "margin": 8,
-                "border_focus": "#268bd2",
-                "border_normal": "#002b36"
-                }
+    "margin": 8,
+    "border_focus": "#268bd2",
+    "border_normal": "#002b36"
+}
 
 
 layouts = [
@@ -166,192 +154,192 @@ layouts = [
     layout.MonadTall(**layout_theme),
     layout.TreeTab(
         font = "Hack Nerd Font",
-         fontsize = 10,
-         sections = ["FIRST", "SECOND", "THIRD", "FOURTH"],
-         section_fontsize = 10,
-         border_width = 2,
-         bg_color = "#002b36",
-         active_bg = "#268bd2",
-         active_fg = "000000",
-         inactive_bg = "#2aa198",
-         inactive_fg = "1c1f24",
-         padding_left = 0,
-         padding_x = 0,
-         padding_y = 5,
-         section_top = 10,
-         section_bottom = 20,
-         level_shift = 8,
-         vspace = 3,
-         panel_width = 200
-       ),
+        fontsize = 10,
+        sections = ["FIRST", "SECOND", "THIRD", "FOURTH"],
+        section_fontsize = 10,
+        border_width = 2,
+        bg_color = "#002b36",
+        active_bg = "#268bd2",
+        active_fg = "000000",
+        inactive_bg = "#2aa198",
+        inactive_fg = "1c1f24",
+        padding_left = 0,
+        padding_x = 0,
+        padding_y = 5,
+        section_top = 10,
+        section_bottom = 20,
+        level_shift = 8,
+        vspace = 3,
+        panel_width = 200
+    ),
 ]
 
 widget_defaults = dict(
     font='Hack',
-    fontsize=12,
+    fontsize=15,
     padding=3,
 )
 extension_defaults = widget_defaults.copy()
 
 colors = [["#002b36", "#002b36"], # panel background
-          ["#3d3f4b", "#434758"], # background for current screen tab
-          # ["#000000", "#000000"], # font color for group names
-          ["#fdf6e3", "#fdf6e3"], # font color for group names
-          ["#268bd2", "#268bd2"], # border line color for current tab
-          ["#268bd2", "#268bd2"], # border line color for 'other tabs' and color for 'odd widgets'
-          ["#2aa198", "#2aa198"], # color for the 'even widgets'
-          # ["#586e75", "#586e75"], # window name
-          ["#268bd2", "#268bd2"], # window name
-          ["#93a1a1", "#93a1a1"]] # backbround for inactive screens
+    ["#3d3f4b", "#434758"], # background for current screen tab
+    # ["#000000", "#000000"], # font color for group names
+    ["#fdf6e3", "#fdf6e3"], # font color for group names
+    ["#268bd2", "#268bd2"], # border line color for current tab
+    ["#268bd2", "#268bd2"], # border line color for 'other tabs' and color for 'odd widgets'
+    ["#2aa198", "#2aa198"], # color for the 'even widgets'
+    # ["#586e75", "#586e75"], # window name
+    ["#268bd2", "#268bd2"], # window name
+    ["#93a1a1", "#93a1a1"]] # backbround for inactive screens
 
 prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
 
 def init_widgets_list():
     widgets_list = [
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 6,
-                    foreground = colors[2],
-                    background = colors[0]
-                    ),
-                widget.GroupBox(
-                    # font = "Ubuntu Bold",
-                    font = "Hack Nerd Font Mono",
-                    fontsize = 30,
-                    margin_y = 3,
-                    margin_x = 0,
-                    padding_y = 5,
-                    padding_x = 3,
-                    borderwidth = 3,
-                    active =  "#fdf6e3" ,
-                    inactive = "#657b83" ,
-                    rounded = False,
-                    highlight_color = colors[1],
-                    highlight_method = "block",
-                    this_current_screen_border = colors[6],
-                    this_screen_border = "#d33682" ,
-                    other_current_screen_border = colors[6],
-                    other_screen_border = colors[4],
-                    foreground = colors[2],
-                    background = colors[0]
-                    ),
-                widget.Prompt(
-                    prompt = prompt,
-                    font = "Hack Bold",
-                    padding = 10,
-                    foreground = colors[3],
-                    background = colors[0]
-                    ),
+        widget.Sep(
+            linewidth = 0,
+            padding = 6,
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.GroupBox(
+            # font = "Ubuntu Bold",
+            font = "Hack Nerd Font Mono",
+            fontsize = 35,
+            margin_y = 3,
+            margin_x = 0,
+            padding_y = 5,
+            padding_x = 3,
+            borderwidth = 3,
+            active =  "#fdf6e3" ,
+            inactive = "#657b83" ,
+            rounded = False,
+            highlight_color = colors[1],
+            highlight_method = "block",
+            this_current_screen_border = colors[6],
+            this_screen_border = "#d33682" ,
+            other_current_screen_border = colors[6],
+            other_screen_border = colors[4],
+            foreground = colors[2],
+            background = colors[0]
+        ),
+        widget.Prompt(
+            prompt = prompt,
+            font = "Hack Bold",
+            padding = 10,
+            foreground = colors[3],
+            background = colors[0]
+        ),
 
-                widget.Sep(
-                    linewidth = 0,
-                    padding = 6,
-                    foreground = colors[2],
-                    background = colors[0]
-                    ),
+        widget.Sep(
+            linewidth = 0,
+            padding = 6,
+            foreground = colors[2],
+            background = colors[0]
+        ),
 
-                widget.WindowName(
-                    foreground = colors[6],
-                    background = colors[0],
-                    fontsize = 14,
-                    padding = 0,
-                    font = "Hack Nerd Font Mono"
-                    ),
-
-
-                widget.Systray(
-                    background = colors[0],
-                    padding = 5
-                    ),
-                widget.TextBox(
-                    text = '',
-                    background = colors[0],
-                    foreground = colors[4],
-                    padding = 0,
-                    fontsize = 37,
-                    font = "Hack Nerd Font Mono"
-                    ),
-                widget.TextBox(
-                    text = "",
-                    foreground = colors[2],
-                    background = colors[4],
-                    padding = 0,
-                    fontsize = 14,
-                    font = "Hack Nerd Font Mono"
-                    ),
-
-                widget.Memory(
-                    foreground = colors[2],
-                    background = colors[4],
-                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e htop')},
-                    padding = 5
-                    ),
+        widget.WindowName(
+            foreground = colors[6],
+            background = colors[0],
+            fontsize = 16,
+            padding = 0,
+            font = "Hack Nerd Font Mono"
+        ),
 
 
-                widget.TextBox(
-                    text = '',
-                    background = colors[4],
-                    foreground = colors[5],
-                    padding = 0,
-                    fontsize = 37,
-                    font = "Hack Nerd Font Mono"
-                    ),
-                widget.TextBox(
-                    text = " Vol:",
-                    foreground = colors[2],
-                    background = colors[5],
-                    padding = 0,
-                    font = "Hack Nerd Font Mono"
-                    ),
+        widget.Systray(
+            background = colors[0],
+            padding = 5
+        ),
+        widget.TextBox(
+            text = '',
+            background = colors[0],
+            foreground = colors[4],
+            padding = 0,
+            fontsize = 37,
+            font = "Hack Nerd Font Mono"
+        ),
+        widget.TextBox(
+            text = "",
+            foreground = colors[2],
+            background = colors[4],
+            padding = 0,
+            fontsize = 14,
+            font = "Hack Nerd Font Mono"
+        ),
 
-                widget.Volume(
-                    foreground = colors[2],
-                    background = colors[5],
-                    padding = 10,
-                    font = "Hack Nerd Font Mono"
-                    ),
-
-                widget.TextBox(
-                    text = '',
-                    background = colors[5],
-                    foreground = colors[4],
-                    padding = 0,
-                    fontsize = 37,
-                    font = "Hack Nerd Font Mono"
-                    ),
-                widget.CurrentLayoutIcon(
-                    custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
-                    foreground = colors[0],
-                    background = colors[4],
-                    padding = 0,
-                    scale = 0.7,
-                    font = "Hack Nerd Font Mono"
-                    ),
-
-                widget.CurrentLayout(
-                    foreground = colors[2],
-                    background = colors[4],
-                    padding = 5
-                    ),
+        widget.Memory(
+            foreground = colors[2],
+            background = colors[4],
+            mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e htop')},
+            padding = 5
+        ),
 
 
-                widget.TextBox(
-                    text = '',
-                    background = colors[4],
-                    foreground = colors[5],
-                    padding = 0,
-                    fontsize = 37,
-                    font = "Hack Nerd Font Mono"
-                    ),
+        widget.TextBox(
+            text = '',
+            background = colors[4],
+            foreground = colors[5],
+            padding = 0,
+            fontsize = 37,
+            font = "Hack Nerd Font Mono"
+        ),
+        widget.TextBox(
+            text = " Vol:",
+            foreground = colors[2],
+            background = colors[5],
+            padding = 0,
+            font = "Hack Nerd Font Mono"
+        ),
 
-                widget.Clock(
-                    foreground = colors[2],
-                    background = colors[5],
-                    format = "%A, %B %d - %H:%M ",
-                    font = "Hack Nerd Font Mono"
-                    ),
+        widget.Volume(
+            foreground = colors[2],
+            background = colors[5],
+            padding = 10,
+            font = "Hack Nerd Font Mono"
+        ),
 
-                ]
+        widget.TextBox(
+            text = '',
+            background = colors[5],
+            foreground = colors[4],
+            padding = 0,
+            fontsize = 37,
+            font = "Hack Nerd Font Mono"
+        ),
+        widget.CurrentLayoutIcon(
+            custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")],
+            foreground = colors[0],
+            background = colors[4],
+            padding = 0,
+            scale = 0.7,
+            font = "Hack Nerd Font Mono"
+        ),
+
+        widget.CurrentLayout(
+            foreground = colors[2],
+            background = colors[4],
+            padding = 5
+        ),
+
+
+        widget.TextBox(
+            text = '',
+            background = colors[4],
+            foreground = colors[5],
+            padding = 0,
+            fontsize = 37,
+            font = "Hack Nerd Font Mono"
+        ),
+
+        widget.Clock(
+            foreground = colors[2],
+            background = colors[5],
+            format = "%A, %B %d - %I:%M %p ",
+            font = "Hack Nerd Font Mono"
+        ),
+
+    ]
     return widgets_list
 
 
@@ -367,8 +355,8 @@ def init_widgets_screen2():
     return widgets_screen2                 # Monitor 2 will display all widgets in widgets_list
 
 def init_screens():
-    return [Screen(bottom=bar.Bar(widgets=init_widgets_screen1(), opacity=1.0, size=20)),
-            Screen(top=bar.Bar(widgets=init_widgets_screen2(), opacity=1.0, size=20))]
+    return [Screen(bottom=bar.Bar(widgets=init_widgets_screen1() , opacity=1.0, size=30)),
+        Screen(top=bar.Bar(widgets=init_widgets_screen2(), opacity=1.0, size=20))]
 
 if __name__ in ["config", "__main__"]:
     screens = init_screens()
@@ -392,15 +380,15 @@ follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
-])
+                                      # Run the utility of `xprop` to see the wm class and name of an X client.
+                                      *layout.Floating.default_float_rules,
+                                      Match(wm_class='confirmreset'),  # gitk
+                                      Match(wm_class='makebranch'),  # gitk
+                                      Match(wm_class='maketag'),  # gitk
+                                      Match(wm_class='ssh-askpass'),  # ssh-askpass
+                                      Match(title='branchdialog'),  # gitk
+                                      Match(title='pinentry'),  # GPG key password entry
+                                  ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
@@ -423,7 +411,6 @@ wmname = "LG3D"
 def start_once():
     home = os.path.expanduser('~')
     subprocess.call([home + '/.config/qtile/autostart.sh'])
-    os.system("touch ~/foobar")
 
 
 
